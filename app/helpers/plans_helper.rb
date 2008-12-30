145b981402a5918de8417bf6af061d78e7c2c4ff
akio0911 <akio0911@gmail.com>
2008-12-30 23:16:07 +0900

    bug fix of def td

--- app/helpers/plans_helper.rb
+++ app/helpers/plans_helper.rb
@@ -12,23 +12,29 @@ module PlansHelper
   end
   def td(plan, i)
     string=""
+
+    if Time.now < (plan.start + 10.minute * i)
+      string += '<td class="past">'
+        string += start_to_string(plan.start, i)
+    elsif (plan.start + 10.minute * (i+1)) < Time.now
+      string += '<td class="future">'
+        string += start_to_string(plan.start, i)
+    else
+      string += '<td class="now">'
+        string += start_to_string(plan.start, i)
+      string += 'now!'
+    end
+    string += "</td>"
+
     if not plan.flag? and i == 0
       editable_time = Time.now+60*60
       if plan.start > editable_time
-        string += "<td style=\"font-size:1.6em;padding:10px;border-bottom:1px solid black;\">"
-        string += start_to_string(plan.start, i)
-        string += " now!" if Time.now.strftime("%d%H") == plan.start.strftime("%d%H")
-        string += "</td>"
         string += "<td style=\"width:420px;font-size:1.6em;padding:10px;border-bottom:1px solid black;\">"
         string += plan.content
         string += "</td>"
         string += "<td>#{ link_to '編集', edit_plan_path(plan)}</td>"
         string += "<td>#{ link_to '表示', plan_path(plan)}</td>"
       else
-        string += "<td style=\"font-size:1.6em;padding:10px;border-bottom:1px solid black;color:#ccc;\">"
-        string += start_to_string(plan.start, i)
-        string += "<span style=\"color:red;\">now!</span>" if Time.now.strftime("%d%H") == plan.start.strftime("%d%H")
-        string += "</td>"
         string += "<td style=\"width:420px;font-size:1.6em;padding:10px;border-bottom:1px solid black;\">"
         string += plan.content
         string += "</td>"
@@ -36,9 +42,6 @@ module PlansHelper
         string += "<td></td>"
       end
     else
-      string += "<td style=\"font-size:1.6em;padding:10px;border-bottom:1px solid black;\">"
-      string += start_to_string(plan.start, i)
-      string += "</td>"
       string += "<td style=\"width:420px;font-size:1.6em;padding:10px;border-bottom:1px solid black;\">"
       string += plan.content
       string += "</td>"
