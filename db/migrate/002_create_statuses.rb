842e0ae78f559bb15ad2a525a9aaf55a94c02939
akio0911 <akio0911@gmail.com>
2008-12-30 20:28:25 +0900

    add twitter status model

---
+++ db/migrate/002_create_statuses.rb
@@ -0,0 +1,23 @@
+# -*- coding: utf-8 -*-
+class CreateStatuses < ActiveRecord::Migration
+  def self.up
+    create_table :statuses do |t|
+      t.column :status_id, :integer # <id>1071821349</id>
+
+      t.column :status_created_at, :datetime # <created_at>Mon Dec 22 06:34:18 +0000 2008</created_at>
+      t.column :user_location, :string # <location>Tokyo, Japan</location>
+      t.column :status_text, :string # <text>Reblog感覚でプレゼン</text>
+      t.column :user_screen_name, :string # <screen_name>yuiseki</screen_name>      
+
+      t.column :float, :latitude # google apiで変換
+      t.column :float, :longitude # google apiで変換
+
+      t.timestamps
+    end
+    add_index :status, [:status_id], :unique=>true
+  end
+
+  def self.down
+    drop_table :statuses
+  end
+end
