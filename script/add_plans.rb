8e0857aeb6f6cfe0559e2ec0b1ae62caee2f5d91
akio0911 <akio0911@gmail.com>
2008-12-31 17:51:09 +0900

    add location list

--- script/add_plans.rb
+++ script/add_plans.rb
@@ -1,6 +1,8 @@
+# -*- coding: utf-8 -*-
 require 'time'
 #start_time = Time.local(0,0,13,31,12,2008,0,0,false,'Asia/Tokyo')#Time.utc(2008, 12, 31, 12, 0, 'Asia/Tokyo')#08/12/31 12:00
-start_time = Time.local(0,0,12,29,12,2008,0,0,false,'Asia/Tokyo')#Time.utc(2008, 12, 31, 12, 0, 'Asia/Tokyo')#08/12/31 12:00
+#start_time = Time.local(0,0,12,29,12,2008,0,0,false,'Asia/Tokyo')#Time.utc(2008, 12, 31, 12, 0, 'Asia/Tokyo')#08/12/31 12:00
+start_time = Time.local(0,0,12,30,12,2008,0,0,false,'Asia/Tokyo')#Time.utc(2008, 12, 31, 12, 0, 'Asia/Tokyo')#08/12/31 12:00
 
 0.upto(47) do |i|
   content = "未定"
